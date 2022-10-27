55520fd1fff765ebd545ea50e081e96ee19f1d40
MeloAguilar <carmelo.aguilar@iesnervion.es>
2022-10-28 00:01:05 +0200

    Agregar archivos de proyecto.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Ejercicio5;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using DAL.Listados;
+using Entities;
+using System.Collections.ObjectModel;
+
+namespace Ejercicio5;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+		//Llamo a la lista de personas y se la mando al listView para que la muestre
+		ObservableCollection<clsPersona> personas = new ObservableCollection<clsPersona>();
+		var listadoPersonas = new listadoPersonas();
+		;
+		foreach(clsPersona persona in listadoPersonas.getListadoCompletoPersonas())
+		{
+			personas.Add(persona);
+		}
+		PersonasListView.ItemsSource = personas;
+	}
+
+}
+
